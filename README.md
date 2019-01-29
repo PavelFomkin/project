663783fcd531e74808e454b0a06fc29404d0cb07
PavelFomkin <pavel_foma@mail.ru>
2019-01-29 23:03:20 +0300

    Create empty boot app

---
+++ README.md
@@ -0,0 +1,2 @@
+# project
+SpringBoot
---
+++ src/main/java/com/historicalclub/HistoricalClubApplication.java
@@ -0,0 +1,14 @@
+package com.historicalclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalClubApplication.class, args);
+	}
+
+}
+
